4848799317f412477b0ae776d407ed3f876e5237
Tshepo Leshabane <ltshepod@pop-os.localdomain>
2021-08-11 17:02:11 +0200

    Recycleview

--- app/src/main/java/com/example/flylettuceautomobilesurgery/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/flylettuceautomobilesurgery/ui/home/HomeFragment.kt
@@ -1,56 +1,38 @@
 package com.example.flylettuceautomobilesurgery.ui.home
 
+
+
+import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
-import android.widget.ImageView
+import android.view.ViewGroup
+import android.widget.LinearLayout
 import android.widget.TextView
-import androidx.appcompat.view.menu.ActionMenuItemView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.flylettuceautomobilesurgery.R
 
-//import android.os.Bundle
-//import android.view.LayoutInflater
-//import android.view.View
-//import android.view.ViewGroup
-//import android.widget.TextView
-//import androidx.fragment.app.Fragment
-//import androidx.lifecycle.Observer
-//import androidx.lifecycle.ViewModelProvider
-//import com.example.flylettuceautomobilesurgery.R
-//
-//class HomeFragment : Fragment() {
-//
-//    private lateinit var homeViewModel: HomeViewModel
-//
-//    override fun onCreateView(
-//        inflater: LayoutInflater,
-//        container: ViewGroup?,
-//        savedInstanceState: Bundle?
-//    ): View? {
-//        homeViewModel =
-//            ViewModelProvider(this).get(HomeViewModel::class.java)
-//        val root = inflater.inflate(R.layout.fragment_home, container, false)
-//        val textView: TextView = root.findViewById(R.id.text_home)
-//        homeViewModel.text.observe(viewLifecycleOwner, Observer {
-//            textView.text = it
-//        })
-//        return root
-//    }
-//}
+class HomeFragment : Fragment() {
 
-class RecyclerViewAdapter : RecyclerViewAdapter<RecyclerViewAdapter.ViewAdapter>(){
+    private var layoutManager : RecyclerView.LayoutManager? = null
+    private var adapter: RecyclerView.Adapter<HomeViewModel.ViewHolder>? = null
 
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val root = inflater.inflate(R.layout.fragment_home,container, false)
+        val recyclerView : RecyclerView = root.findViewById(R.id.recyclerView)
+//        layoutManager = LinearLayout(this)
+        recyclerView.layoutManager = layoutManager
+        adapter = HomeViewModel ()
+        recyclerView.adapter = adapter
 
-    private val itemTitles = arrayOf("get quote","services")
-
-    private val itemDetails = arrayOf("ruest quote","check our services")
-    inner class ViewHolder (itemView: View) : RecyclerView.ViewHolder(itemView) {
-        var image: ImageView
-        var textTitle: TextView
-        var textDes: TextView
-
-        init {
-            image = itemView.findViewById(R.id.item_image)
-        textTitle=itemView.findViewById(R.id.card_title)
-        textDes = itemView.findViewById(R.id.card_details)}
+        return root
     }
 }
+
--- app/src/main/java/com/example/flylettuceautomobilesurgery/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/example/flylettuceautomobilesurgery/ui/home/HomeViewModel.kt
@@ -1,13 +1,55 @@
 package com.example.flylettuceautomobilesurgery.ui.home
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
 
-class HomeViewModel : ViewModel() {
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.view.menu.ActionMenuItemView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.flylettuceautomobilesurgery.R
 
-    private val _text = MutableLiveData<String>().apply {
-        value = "This is home Fragment"
+
+class HomeViewModel : RecyclerView.Adapter<HomeViewModel.ViewHolder>(){
+
+
+    private val itemTitles = arrayOf("get quote","services")
+
+    private val itemDetails = arrayOf("request quote","check our services")
+
+    private val itemImage = arrayOf(R.mipmap.ic_launcher_round,R.mipmap.ic_launcher_foreground)
+
+
+    inner class ViewHolder (itemView: View) : RecyclerView.ViewHolder(itemView) {
+        var image: ImageView
+        var textTitle: TextView
+        var textDes: TextView
+
+        init {
+            image = itemView.findViewById(R.id.item_image)
+            textTitle=itemView.findViewById(R.id.card_title)
+            textDes = itemView.findViewById(R.id.card_details)}
     }
-    val text: LiveData<String> = _text
+
+    override fun onCreateViewHolder(parent: ViewGroup,viewType: Int) :ViewHolder {
+        val v: View = LayoutInflater.from(parent.context).inflate(R.layout.card_view_model,parent,false)
+        return ViewHolder(v)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder,position: Int) {
+        holder.textTitle.text = itemTitles [position]
+        holder.textDes.text = itemDetails [position]
+        holder.image.setImageResource(itemImage [position])
+
+        holder.itemView.setOnClickListener{ v: View ->
+
+            Toast.makeText(v.context,"Clicked on the item", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return itemTitles.size
+    }
 }
